cdd0360e85479125fcaa98341ee20f4f342d48e9
최하영 <xxhayoxx@naver.com>
2023-07-19 15:56:41 +0900

    동적 open graph 추가

---
+++ src/app/detail/[id]/opengraph-image.tsx
@@ -0,0 +1,38 @@
+import { ImageResponse } from 'next/server'
+ 
+export const runtime = 'edge'
+ 
+export const alt = 'ha0peno'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+export const contentType = 'image/png'
+ 
+export default async function Image({ params }: { params: { id: string } }) {
+  const url = 'https://ha0.work/api/get/detail?id=' + params.id;
+  const post = await fetch(url).then((res) =>
+    res.json()
+  )
+ 
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 48,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {post.title}
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
