aa3abfea87d3268bcde9ab6d01a2c39b37245e0e
최하영 <xxhayoxx@naver.com>
2023-07-19 16:27:31 +0900

    동적 opengraph image 수정

--- src/app/detail/[id]/opengraph-image.tsx
+++ src/app/detail/[id]/opengraph-image.tsx
@@ -18,33 +18,20 @@ export default async function Image({ params }: { params: { id: string } }) {
  
   return new ImageResponse(
     (
-      <div>
-        <div
-          style={{
-            backgroundImage: `url(${post.thumbnail})`,
-            filter: 'blur(8px)',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            width: '100%',
-            height: '100%'
-          }}
-        >
-        </div>
-        <div
-          style={{
-          fontSize: 100,
-          fontWeight: 'bold',
-          color: '#fff',
+      <div
+        style={{
+          backgroundImage: `url(${post.thumbnail})`,
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
           width: '100%',
           height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
+          fontSize: 100,
+          fontWeight: 'bold',
+          color: '#fff'
         }}
-        >
-          {post.title}
-        </div>
+      >
+        { post.title }
       </div>
     ),
     {
