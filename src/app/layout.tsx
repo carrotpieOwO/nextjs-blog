c22dbb358fb50dbcc51a88934282d52be7c090d5
최하영 <xxhayoxx@naver.com>
2023-07-20 16:27:12 +0900

    naver 소유권 등록

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,6 +73,7 @@ export default async function RootLayout({children,}: { children: React.ReactNod
   return (
     <html lang="en">
       <meta name="google-site-verification" content="aqC8RjvcEAuRUmytJpYNa0P1hZsJkxAi74fJXN0xFgc" />
+      <meta name="naver-site-verification" content="98cd3a01dfd80547bcafef1b1b2686c6d302ca33" />
       <body className={notoSansKr.className} style={{ overflowX: 'hidden' }}>
         <Providers>
           <ReactQueryProvider>
