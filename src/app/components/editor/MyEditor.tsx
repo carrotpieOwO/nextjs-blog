4b4064e336e9a45962736da47f78684379410ac0
최하영 <xxhayoxx@naver.com>
2023-08-02 17:24:03 +0900

    editor code highlight 플러그인 추가

--- src/app/components/editor/MyEditor.tsx
+++ src/app/components/editor/MyEditor.tsx
@@ -7,6 +7,10 @@ import { storage } from '@/util/firebase/config';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { PreviewStyle } from '@toast-ui/editor';
 import { Images } from '@/util';
+import 'prismjs/themes/prism.css'
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight'
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css'
+import Prism from 'prismjs'
 
 const toolbarItems = [
     ['heading', 'bold', 'italic', 'strike'],
@@ -14,7 +18,7 @@ const toolbarItems = [
     ['ul', 'ol', 'task'],
     ['table', 'link'],
     ['image'],
-    ['code'],
+    ['code', 'codeblock'],
     ['scrollSync'],
 ];
 
@@ -85,7 +89,7 @@ export default function MyEditor({ editorRef, images, initialValue }: Props) {
                   usageStatistics={false}
                   toolbarItems={toolbarItems}
                   useCommandShortcut={true}
-                  plugins={[colorSyntax]}
+                  plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]} // 추가!
                   hooks={{ addImageBlobHook: onUploadImage }}
                 />
               </>
